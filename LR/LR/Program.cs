841e1bac57fc42fc404c85014374931d65f09618
Cristina-Andrada Vinter <vinter.cristinaandrada@student.uoradea.ro>
2022-06-03 15:06:21 +0300

    Add project files.

---
+++ LR/LR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
