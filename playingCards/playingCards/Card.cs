4060a767a378b733baef3d994cefd3eaf34f5cbf
Vinter Andrada <vinterandrada08@gmail.com>
2022-06-11 16:09:18 +0300

    finished

--- playingCards/playingCards/Card.cs
+++ playingCards/playingCards/Card.cs
@@ -7,9 +7,9 @@ namespace playingCards
     public enum Suit
     {
         Club=1,
-        Diamond=2,
-        Heart=3,
-        Spade=4,
+        Diamond,
+        Heart,
+        Spade,
     }
     public enum Rank
     {
